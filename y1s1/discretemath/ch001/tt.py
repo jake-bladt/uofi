dda4d31ce1705f9e6123382ca828e8f16cb89b13
Jake Bladt <jake@ambulnz.com>
2017-05-04 17:58:57 -0400

    +characterize

--- y1s1/discretemath/ch001/tt.py
+++ y1s1/discretemath/ch001/tt.py
@@ -1,13 +1,41 @@
 #!/usr/bin/env python
+# Create a truth table for a logical expression.
+# The following are valid elements:
+#   Any lower-case letter: independent variable
+#   ! not
+#   -> if... then
+#   <-> if and only if
+#   ^ and
+#   | or
+# Expressions are evaluated left to right.
+# Parentheses are not supported.
+
 import re
 import sys
 
+UNRECOGNIZED = 0
+LETTER = 1
+SYMBOL = 2
+
+def categorize(char):
+  if is_letter(char) return LETTER
+  return UNRECOGNIZED
+
+def is_letter(char):
+  return re.match('[a-z]', char)
+
 def tokenize(expression):
   ret = []
+  oper_buffer = []
+
   any_whitespace = re.compile('\s')
   working_exp = re.sub(any_whitespace, '', expression)
   wexp_arr = list(working_exp)
-  print len(wexp_arr)
+  for c in wexp_arr:
+    cat = categorize(c)
+    if UNRECOGNIZED = cat:
+      print 'Unrecognized character ' + c
+      sys.exit(-1) 
 
   return ret
 
