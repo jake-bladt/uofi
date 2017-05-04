327072318d2a35d7ecc07b4779c106ac5fd700db
Jake Bladt <jake@ambulnz.com>
2017-05-04 17:40:37 -0400

    -whitespace

--- y1s1/discretemath/ch001/tt.py
+++ y1s1/discretemath/ch001/tt.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
-
+import re
 import sys
 
+def tokenize(expression):
+  ret = []
+  any_whitespace = re.compile('\s')
+  working_exp = re.sub(any_whitespace, '', expression)
+  print working_exp
+
+  return ret
+
 if len(sys.argv) != 2:
-  print 'USAGE: tt.py <expression>'
+  print 'USAGE: tt.py "<expression>"'
   sys.exit(-1)
+
+ast = tokenize(sys.argv[1])
