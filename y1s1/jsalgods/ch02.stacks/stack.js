2ab8b34daa5e6d52cf2bfea542d72ad7e09a4ae8
Jake Bladt <jake@ambulnz.com>
2017-05-08 15:30:19 -0400

    +morefuncs

--- y1s1/jsalgods/ch02.stacks/stack.js
+++ y1s1/jsalgods/ch02.stacks/stack.js
@@ -4,4 +4,8 @@ function Stack() {
   this.push = (e) => items.push(e);
   this.pop = (e) => items.pop(e);
   this.peek = () => items.length > 0 ? items[items.length -1] : undefined;
+  this.isEmpty = () => items.length === 0;
+  this.size = () => items.length;
+  this.clear = () => items = [];
+  this.print = () => console.log(items);
 }
