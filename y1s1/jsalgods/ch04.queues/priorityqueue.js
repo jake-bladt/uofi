e46c45dad7e57f06e7b58993faee224f26a695fe
Jake Bladt <jake@ambulnz.com>
2017-05-08 16:42:45 -0400

    +enqueue

--- y1s1/jsalgods/ch04.queues/priorityqueue.js
+++ y1s1/jsalgods/ch04.queues/priorityqueue.js
@@ -1,9 +1,24 @@
 function PriorityQueue() {
+
+  let items = [];
+
   function QueueElement(elem, priority) {
     this.element = elem;
     this.priority = priority;
   }
 
+  this.enqueue = function(item, priority) {
+    let qe = new QueueElement(item, priority);
+    let added = false;
 
-   
+    for(let i = 0; i < items.length; i++) {
+      if(qe.priority < items[i].priority) {
+        items.splice(i, 0, qe);
+        added = true;
+        break;
+      }
+    }
+    if(!added) items.push(qe);
+  }
+
 }
