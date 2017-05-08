dbe574bfb610e8ad3f02e02cb243dda5f95d58ac
Jake Bladt <jake@ambulnz.com>
2017-05-08 17:06:10 -0400

    +append

--- y1s1/jsalgods/ch05.linkedlists/ll.js
+++ y1s1/jsalgods/ch05.linkedlists/ll.js
@@ -1,3 +1,24 @@
 function LinkedList() {
-    
+  function Node(elem) => {
+    this.element = elem;
+    this.next = null;
+  }
+
+  let length = 0;
+  let head = null;
+  let last = null;
+
+  this.append = (elem) => {
+    let node = Node(elem);
+    if(head === null) {
+      head = node;
+    } else {
+      last.next = node; 
+    }
+    last = node;
+    length++;
+  }
+
+
+
 }
