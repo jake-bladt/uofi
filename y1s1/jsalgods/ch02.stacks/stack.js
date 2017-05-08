689af3312f5ab0231408bb708f85eeb016194fc3
Jake Bladt <jake@ambulnz.com>
2017-05-08 15:30:57 -0400

    -unused e

--- y1s1/jsalgods/ch02.stacks/stack.js
+++ y1s1/jsalgods/ch02.stacks/stack.js
@@ -2,7 +2,7 @@ function Stack() {
   let items = [];
 
   this.push = (e) => items.push(e);
-  this.pop = (e) => items.pop(e);
+  this.pop = () => items.pop();
   this.peek = () => items.length > 0 ? items[items.length -1] : undefined;
   this.isEmpty = () => items.length === 0;
   this.size = () => items.length;
