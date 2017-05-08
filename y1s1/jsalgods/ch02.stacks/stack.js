556ecacd13a08026182af8ea015fa9fbc3571891
Jake Bladt <jake@ambulnz.com>
2017-05-08 15:27:00 -0400

    +peek

--- y1s1/jsalgods/ch02.stacks/stack.js
+++ y1s1/jsalgods/ch02.stacks/stack.js
@@ -2,5 +2,6 @@ function Stack() {
   let items = [];
 
   this.push = (e) => items.push(e);
-  this.pop = (e) => items.push(e);
+  this.pop = (e) => items.pop(e);
+  this.peek = () => items.length > 0 ? items[items.length -1] : undefined;
 }
